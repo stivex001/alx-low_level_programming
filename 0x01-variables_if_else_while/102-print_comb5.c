37554c4293f162343c4d00f1d3ef9b894a466dc2
Adeyemo Stephen <87942124+stivex001@users.noreply.github.com>
2022-03-11 21:19:54 +0000

    C code

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,1 +1,34 @@
-102-print_comb5.c
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations
+ *
+ * Return: 0 on success.
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = 0; j <= 99; j++)
+		{
+			if (i < j && i != j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
