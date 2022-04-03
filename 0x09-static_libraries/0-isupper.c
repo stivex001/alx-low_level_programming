c2fa828675067ea20d0748af8e7d9a6d49dd056c
Adeyemo Stephen <87942124+stivex001@users.noreply.github.com>
2022-04-03 15:45:00 +0100

    static

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ *_isupper - a function that checks for uppercase characters
+ *@c: a variable that stores letters
+ *Return: Returns a value of 1 if the variable c contains an uppercase
+ *letter.
+ */
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
@@ -10,5 +17,4 @@ int _isupper(int c)
 	{
 		return (0);
 	}
-
-}	
+}
