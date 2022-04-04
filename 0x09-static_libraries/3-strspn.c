c0fa8925c90a976291ac6b0db803160b1704997a
Adeyemo stephen <stephenadeyemo@gmail.com>
2022-04-04 16:20:01 -0700

    static

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	int c;
+
+	i = 0;
+	c = 0;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+
+			if (s[i] == accept[j])
+			{
+				c++;
+				break;
+			}
+			j++;
+		}
+		if (accept[j] == '\0')
+			break;
+		i++;
+	}
+	return (c);
+
+}
