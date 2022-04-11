9d4f9c5a74d36cbdcd9780a16af299e333272326
Adeyemo Stephen <87942124+stivex001@users.noreply.github.com>
2022-04-11 19:39:26 +0100

    structures

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,1 +1,18 @@
-i
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free dog memory
+ * @d: dog
+ */
+
+
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+	return;
+
+free(d->owner);
+free(d->name);
+free(d);
+}
